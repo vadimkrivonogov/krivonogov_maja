c93a4d357aacc5f42b7e9ba89a78caf8d2325705
vadimkrivonogov <120089193+vadimkrivonogov@users.noreply.github.com>
2023-04-26 11:51:41 +0300

    vadimkrivoo.py

--- vadimkrivoo.py
+++ vadimkrivoo.py
@@ -1,21 +1,24 @@
-﻿import pygame
-import random
+import pygame
+import random 
 import sys
 
 def Maja(x,y,laius,kõrgus,pind,värv):
     punktid=[(x,y-((3/4.0)*kõrgus)), (x,y), (x+laius,y), (x+laius,y-(3/4.0)*(kõrgus)),(x,y-((3/4.0)*kõrgus)), (x+laius/2.0,y-kõrgus),(x+laius,y-(3/4.0)*kõrgus)]
+
     suurus=random.randint(1,10)
     pygame.draw.lines(pind,värv,False,punktid,suurus)
 
-def Uks(x,y, laius, kõrgus, pind,värv):
-    punktid=[(x,y),(x,(1/2)*kõrgus),(x+(1/3)*laius,y-(1/2)*kõrgus),(x+(1/3)*laius,y),(x,y)]
+def Uks(x,y,laius,kõrgus,pind,värv):
+    punktid=[(x,y),(x,y-(1/2)*kõrgus),(x+(1/3)*laius,y-(1/2)*kõrgus),(x+(1/3)*laius,y),(x,y)]
+    
     suurus=random.randint(1,10)
-    pygame.draw.lines(pind,värv,False,punktid,suurus)
+    pygame.draw.lines(pind,värv,True,punktid,suurus)
 
-def Aken(x,y, laius, kõrgus, pind,värv):
-    punktid=[(x,y),(x,(1/2)*kõrgus),(x+(1/3)*laius,y-(1/2)*kõrgus),(x+(1/2)*laius,y), (x,y)]
+def aken(x,y,laius,kõrgus,pind,värv):
+    punktid=[(x,y),(x,y-kõrgus),(x+laius,y-kõrgus),(x+laius,y),(x,y)]
+
     suurus=random.randint(1,10)
-    pygame.draw.lines(pind,värv,False,punktid,suurus)
+    pygame.draw.lines(pind,värv,True,punktid,suurus)
 
 r=random.randint(0,255)
 g=random.randint(0,255)
@@ -25,16 +28,36 @@ fon=[r,g,b]
 r=random.randint(0,255)
 g=random.randint(0,255)
 b=random.randint(0,255)
-majavärv=[r,g,b]
-
+majavarv=[r,g,b]
 pind=pygame.display.set_mode([640,480])
-pygame.display.set_caption("Juhustikud objektid")
-pind.fill(fon)
+pygame.display.set_caption("Juhustikudobjektid")
+pind.fill(fon) 
 
-#сокращения
-Maja(100,400,300,400,pind,majavärv)
-Uks(100,400,500,400,pind,majavärv)                            
-Aken(100,400,200,300, pind,majavärv)
+
+
+Maja(100,400,300,400,pind,majavarv)
+Uks(100,400,300,400,pind,majavarv)
+aken(250,250,100,100,pind,majavarv)
+pygame.display.flip()
+
+""" старое окно и дверь 
+ristkülik=pygame.Rect(300,220,100,180)                            
+pygame.draw.rect(pind,(255,255,255),ristkülik) #дверь
+
+ristkülik=pygame.Rect(370,300,15,15)                            
+pygame.draw.rect(pind,(0,0,0),ristkülik)#ручка
+
+ristkülik=pygame.Rect(130,170,105,105)                            
+pygame.draw.rect(pind,(255,255,0),ristkülik) #bg okna
+
+ristkülik=pygame.Rect(180,175,5,103)                            
+pygame.draw.rect(pind,(0,0,0),ristkülik) #okno x
+
+ristkülik=pygame.Rect(130,223,105,5)                            
+pygame.draw.rect(pind,(0,0,0),ristkülik) #okno y
+"""
+
+
 
 
 for i in range(10):
@@ -42,17 +65,13 @@ for i in range(10):
     r=random.randint(0,255)
     g=random.randint(0,255)
     b=random.randint(0,255)
-    
-    #размер
+    #suurus
     laius=random.randint(1,80)
     kõrgus=random.randint(1,80)
-    
-    #место
+    #asukoht
     x=random.randint(0,640-laius)
     y=random.randint(0,480-kõrgus)
     pygame.draw.rect(pind,varv,[x,y,laius,kõrgus])
-
-    
     pygame.display.flip()
 while True:
     event=pygame.event.poll()
